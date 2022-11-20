9d9b9768552cb4486e151ee908bc741d282c1baa
ABDUL RASID <abdulrasid@ABDULs-MacBook-Pro.local>
2022-11-20 22:28:17 +0530

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,8 @@ const Home = () => {
             </IconButton>
         </Box>
        
-        <video src={BgVideo} autoPlay='autoplay' playsinline muted={true} loop={true}/>
+        {/* <video src={BgVideo} autoPlay='autoplay' playsinline muted={true} loop={true}/> */}
+        <video src={BgVideo} muted="muted" autoplay="autoplay" playsinline="playsinline" loop="loop" />
        
         <div className='overlay'></div>
         <Box sx={{position:'absolute',top:'0', width:'100%',height:'100%', display:'flex',flexDirection:'column', zIndex:'555', color:'#fff', alignItems:'center', justifyContent:'center'}}>
