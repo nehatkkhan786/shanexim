7e1652484c49a789fa74da56856d34ef16088c9d
ABDUL RASID <abdulrasid@ABDULs-MacBook-Pro.local>
2022-11-20 18:59:20 +0530

    initial commit with navbar and landing page

---
+++ src/componenets/SidebarDrawer.jsx
@@ -0,0 +1,40 @@
+import { Box, List, ListItem, ListItemButton, ListItemText, SwipeableDrawer } from '@mui/material'
+import React from 'react'
+
+const SidebarDrawer = ({open, setOpen}) => {
+    const iOS =
+    typeof navigator !== "undefined" &&
+    /iPad|iPhone|iPod/.test(navigator.userAgent);
+  return (
+    <SwipeableDrawer
+    PaperProps={{
+        sx: {
+          backgroundColor: "#2D5A27",
+          color: "white",
+          borderRadius: "100px 0px 0 100px",
+        },
+      }}
+    anchor='right'
+    disableBackdropTransition={!iOS}
+    disableDiscovery={iOS} 
+    open={open}
+    onClose={()=>setOpen(false)}
+    onOpen = {()=>setOpen(true)}
+    >
+    <Box sx={{width:{xs:250, md:300}, display:'flex', justifyContent:'center'}}>
+        <Box sx={{display:'flex', justifyContent:'center'}}>
+            <List>
+               <ListItem disablePadding sx={{':hover':{bgcolor:'green'}}}>
+                   <ListItemButton>
+                       <ListItemText>Home</ListItemText>
+                   </ListItemButton>
+               </ListItem>
+            </List>
+        </Box>
+    </Box>
+
+    </SwipeableDrawer>
+  )
+}
+
+export default SidebarDrawer
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import BgVideo from '../assets/video2.mp4'
+import { Box, Button, IconButton, Typography } from '@mui/material';
+import { SocialIcon } from 'react-social-icons';
+import './home.css'
+import { height } from '@mui/system';
+import { useState } from 'react';
+import SidebarDrawer from '../componenets/SidebarDrawer';
+
+const Home = () => {
+    const [open, setOpen] = useState(false)
+
+
+  return (
+    <Box sx={{width:'100%',height:'100vh' }}>
+        <Box sx={{position:'absolute', zIndex:'999', display:'flex', justifyContent:'space-between', width:'100%', padding:{md:'20px 70px', xs:'20px 10px', color:'#fff'}}}>
+           <Typography variant='h4' sx={{cursor:'pointer'}}>ShanExim</Typography>
+            <IconButton onClick={()=>setOpen(!open)}>
+                <MenuRoundedIcon sx={{color:'#fff'}}/>
+            </IconButton>
+        </Box>
+        <video src={BgVideo} autoPlay muted loop/>
+        <div className='overlay'></div>
+        <Box sx={{position:'absolute',top:'0', width:'100%',height:'100%', display:'flex',flexDirection:'column', zIndex:'555', color:'#fff', alignItems:'center', justifyContent:'center'}}>
+            <Typography sx={{ typography:{xs:'h4',md:'h3'}, textTransform:'uppercase', fontWeight:700,}}>WorldClass Seed </Typography>
+            <Typography sx={{ typography:{xs:'h5',md:'h4'}, mt:1, textTransform:'uppercase', fontWeight:500 }}>From North-East India</Typography>
+            <Typography sx={{ypography:{xs:'title2',md:'title', textAlign:'center',}, mt:1}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut </Typography>
+            <Button  variant='contained'sx={{mt:2, color:'white', backgroundColor:'green', transition:'0.3s', ':hover':{letterSpacing:'10px', backgroundColor:'green'}}}>Explore</Button>
+        </Box>
+        <SidebarDrawer open={open} setOpen={setOpen}/>
+    </Box>
+  )
+}
+
+export default Home 
